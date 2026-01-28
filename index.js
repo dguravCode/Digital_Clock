87e5cb3fbc39d98e480994dd41163d37cc3458e2
demo-project <dattaramgurav1997@gmail.com>
2026-01-28 14:01:54 +0530

    JS-first-project

---
+++ index.js
@@ -0,0 +1,17 @@
+
+setInterval(() => {
+  const ele = document.querySelector(".clock");
+
+  const time = new Date();
+  const options = {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  ele.textContent = time.toLocaleTimeString("en-US", options);
+  sound.play();
+}, 1000);
+
+const sound = document.querySelector('audio');
